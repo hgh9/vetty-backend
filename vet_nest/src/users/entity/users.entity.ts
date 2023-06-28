f607c3433fa8e5f968767c0419874f46abdd13a0
hayasha <hayashasong@gmail.com>
2023-06-28 18:46:56 +0900

    ADD signup process and test

---
+++ vet_nest/src/users/entity/users.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  userName: string;
+
+  @Column()
+  phoneNumber: string;
+}
--- vet_nest/src/users/users.service.ts
+++ vet_nest/src/users/users.service.ts
@@ -1,19 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entity/users.entity';
+import { CreateUserDto } from './dto/createUser.dto';
 
 @Injectable()
 export class UsersService {
-  public validateEmail(email: string) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async signup(createUserDto: CreateUserDto) {
+    if (!this.isCreateUserDtoValid(createUserDto)) {
+      throw new HttpException('Invalid user data', HttpStatus.BAD_REQUEST);
+    }
+    if (await this.isUserDuplicate(createUserDto.email)) {
+      throw new HttpException('Duplicate user data', HttpStatus.BAD_REQUEST);
+    }
+
+    const newUser = this.userRepository.create(createUserDto);
+    await this.userRepository.save(newUser);
+    return newUser;
+  }
+
+  private async isUserDuplicate(email) {
+    return !!(await this.userRepository.findOneBy({ email }));
+  }
+
+  private isCreateUserDtoValid(body: CreateUserDto) {
+    return (
+      this.isEmailValid(body.email) &&
+      this.isUserNameValid(body.userName) &&
+      this.isPasswordValid(body.password) &&
+      this.isPhoneNumberValid(body.phoneNumber)
+    );
+  }
+
+  private isEmailValid(email: string) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
 
-  public validatePassword(password: string) {
+  private isPasswordValid(password: string) {
     const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,16}$/;
     return passwordRegex.test(password);
   }
 
-  public validateName(name: string) {
+  private isUserNameValid(userName: string) {
     const koreanNameRegex = /^[가-힣]{2,6}$/;
-    return koreanNameRegex.test(name);
+    return koreanNameRegex.test(userName);
+  }
+
+  private isPhoneNumberValid(phoneNumber: string) {
+    const phoneNumberRegex = /^01\d{9}$/;
+    return phoneNumberRegex.test(phoneNumber);
   }
 }
