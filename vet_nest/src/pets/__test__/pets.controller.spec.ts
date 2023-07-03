11feec11c4d83e389ac479e3daa3cf2e6ec99e2e
hayasha <hayashasong@gmail.com>
2023-07-04 07:28:54 +0900

    ADD pets controller

--- vet_nest/src/pets/__test__/pets.controller.spec.ts
+++ vet_nest/src/pets/__test__/pets.controller.spec.ts
@@ -1,18 +1,192 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PetsController } from '../pets.controller';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
 
-describe('PetsController', () => {
-  let controller: PetsController;
+const LOCAL_HOST = 'http://localhost:3001';
+
+describe('LIST PET', () => {
+  let app: INestApplication;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [PetsController],
-    }).compile();
+    const module: TestingModule = await Test.createTestingModule({}).compile();
 
-    controller = module.get<PetsController>(PetsController);
+    app = module.createNestApplication();
+    await app.init;
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  test('GET /pets lists pets of the user', () => {
+    const userId = 1;
+
+    request(LOCAL_HOST)
+      .get(`/pets?userId=${userId}`)
+      .then((res: request.Response) => {
+        expect(res.statusCode).toEqual(HttpStatus.OK);
+        //expect(res.body.result).toEqual(something);
+      });
+  });
+
+  test('GET /pets fails when userId is not given', () => {
+    const userId = null;
+
+    request(LOCAL_HOST)
+      .get(`/pets?userId=${userId}`)
+      .then((res: request.Response) => {
+        expect(res.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+        expect(res.body.result).toBeFalsy();
+        expect(res.body.message).toEqual('userId is required');
+      });
+  });
+});
+
+describe('ADD PET', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({}).compile();
+
+    app = module.createNestApplication();
+    await app.init;
+  });
+
+  test('POST /pets adds a pet of the user', () => {
+    const addPetDto = {
+      name: 'goyangyi',
+      species: 'CAT',
+      birth: '2022-01-01',
+      weightKg: 5,
+      breed: 'Russian Blue',
+      age: 1,
+      allergy: null,
+      illness: null,
+      neutered: true,
+      userId: 1,
+    };
+
+    request(LOCAL_HOST)
+      .post(`/pets`)
+      .send(addPetDto)
+      .then((res: request.Response) => {
+        expect(res.statusCode).toEqual(HttpStatus.CREATED);
+        //expect(res.body.result).toEqual(something);
+      });
+  });
+
+  test('POST /pets fails when required data are missing', () => {
+    const addPetDto = {
+      name: 'goyangyi',
+      species: null,
+      birth: '2022-01-01',
+      weightKg: 5,
+      breed: 'Russian Blue',
+      age: 1,
+      allergy: null,
+      illness: null,
+      neutered: true,
+      userId: 1,
+    };
+
+    request(LOCAL_HOST)
+      .post(`/pets`)
+      .send(addPetDto)
+      .then((res: request.Response) => {
+        expect(res.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+        expect(res.body.result).toBeFalsy();
+        expect(res.body.message).toEqual('valid pet data are required');
+      });
+  });
+});
+
+describe('UPDATE PET', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({}).compile();
+
+    app = module.createNestApplication();
+    await app.init;
+  });
+
+  test('PUT /pets updates a pet of the user', () => {
+    const updatePetDto = {
+      petId: 1,
+      name: 'goyangyi',
+      species: 'CAT',
+      birth: '2022-01-01',
+      weightKg: 5,
+      breed: 'Russian Blue',
+      age: 1,
+      allergy: null,
+      illness: null,
+      neutered: true,
+      userId: 1,
+    };
+
+    request(LOCAL_HOST)
+      .put(`/pets`)
+      .send(updatePetDto)
+      .then((res: request.Response) => {
+        expect(res.statusCode).toEqual(HttpStatus.OK);
+        //expect(res.body.result).toEqual(something);
+      });
+  });
+
+  test('PUT /pets fails when required data are missing', () => {
+    const updatePetDto = {
+      petId: 1,
+      name: 'goyangyi',
+      species: null,
+      birth: '2022-01-01',
+      weightKg: 5,
+      breed: 'Russian Blue',
+      age: 1,
+      allergy: null,
+      illness: null,
+      neutered: true,
+      userId: 1,
+    };
+
+    request(LOCAL_HOST)
+      .put(`/pets`)
+      .send(updatePetDto)
+      .then((res: request.Response) => {
+        expect(res.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+        expect(res.body.result).toBeFalsy();
+        expect(res.body.message).toEqual('valid pet data are required');
+      });
+  });
+});
+
+describe('DELETE PET', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({}).compile();
+
+    app = module.createNestApplication();
+    await app.init;
+  });
+
+  test('DELETE /pets deletes the pet', () => {
+    const petId = 1;
+
+    request(LOCAL_HOST)
+      .delete(`/pets?petId=${petId}}`)
+      .then((res: request.Response) => {
+        expect(res.statusCode).toEqual(HttpStatus.OK);
+        //expect(res.body.result).toEqual(something);
+      });
+  });
+
+  test('DELETE /pets fails when petId is not given', () => {
+    const petId = null;
+
+    request(LOCAL_HOST)
+      .delete(`/pets?petId=${petId}`)
+      .then((res: request.Response) => {
+        expect(res.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+        expect(res.body.result).toBeFalsy();
+        expect(res.body.message).toEqual('petId is required');
+      });
   });
 });
--- vet_nest/src/pets/pets.controller.ts
+++ vet_nest/src/pets/pets.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  HttpException,
+  Query,
+  Body,
+} from '@nestjs/common';
+import { PetsService } from './pets.service';
 
 @Controller('pets')
-export class PetsController {}
+export class PetsController {
+  constructor(private PetsService: PetsService) {}
+
+  @Get()
+  async listPets(@Query('userId') userId: number) {
+    const result = await this.PetsService.listPet(userId).catch((error) => {
+      if (error.message === 'userId is required') {
+        throw new HttpException({ result: false, message: error.message }, 400);
+      }
+    });
+
+    return { result: result, code: 200, message: '/pets' };
+    // TODO: general response class
+  }
+
+  @Post()
+  async addPet(@Body() addPetDto) {
+    const result = await this.PetsService.addPet(addPetDto).catch((error) => {
+      if (error.message === 'valid pet data are required') {
+        throw new HttpException({ result: false, message: error.message }, 400);
+      }
+    });
+
+    return { result: result, code: 201, message: '/pets' };
+  }
+
+  @Put()
+  async updatePet(@Body() updatePetDto) {
+    const result = await this.PetsService.updatePet(updatePetDto).catch((error) => {
+      if (error.message === 'valid pet data are required') {
+        throw new HttpException({ result: false, message: error.message }, 400);
+      }
+    });
+
+    return { result: result, code: 200, message: '/pets' };
+  }
+
+  @Delete()
+  async deletePets(@Query('petId') petId: number) {
+    const result = await this.PetsService.deletePet(petId).catch((error) => {
+      if (error.message === 'petId is required') {
+        throw new HttpException({ result: false, message: error.message }, 400);
+      }
+    });
+
+    return { result: result, code: 200, message: '/pets' };
+  }
+}
